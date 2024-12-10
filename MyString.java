8c6cb969f0b599259a0bd13dbfb38a08e87f6185
john-smith8888 <johnsmith8aug88@gmail.com>
2024-12-10 13:15:57 +0200

    mystring 3rd update - fixed single letter spacedString

--- MyString.java
+++ MyString.java
@@ -88,6 +88,8 @@ public class MyString {
      * @return a string consisting of the characters of str, separated by spaces.
      */
     public static String spacedString(String str) {
+        if(str.length()<=1)
+            return str;
         String spaced = "";
         for (int i=0;i<str.length()-1;i++){
             spaced += str.charAt(i) + " ";
